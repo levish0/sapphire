147cb21e4e64f8952d5b351eb484639e9d7e3eee
levish0 <shiueo.csh@gmail.com>
2026-03-27 17:40:35 +0900

    misc

--- sapphire-server/crates/sapphire_dto/src/user/request/get_user_profile.rs
+++ sapphire-server/crates/sapphire_dto/src/user/request/get_user_profile.rs
@@ -1,4 +1,4 @@
-use crate::validator::string_validator::validate_not_blank;
+use crate::validator::string_validator::{validate_handle, validate_not_blank};
 use serde::Deserialize;
 use utoipa::{IntoParams, ToSchema};
 use validator::Validate;
@@ -8,10 +8,11 @@ use validator::Validate;
 /// Request payload for get user profile request.
 pub struct GetUserProfileRequest {
     #[validate(length(
-        min = 3,
-        max = 20,
-        message = "Handle must be between 3 and 20 characters."
+        min = 4,
+        max = 15,
+        message = "Handle must be between 4 and 15 characters."
     ))]
     #[validate(custom(function = "validate_not_blank"))]
+    #[validate(custom(function = "validate_handle"))]
     pub handle: String,
 }
