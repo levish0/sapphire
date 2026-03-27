a9a5782d54abb2ad24480a85651aa76476744699
levish0 <shiueo.csh@gmail.com>
2026-03-27 15:47:34 +0900

    components

---
+++ sapphire-ui/src/lib/components/ui/avatar/index.ts
@@ -0,0 +1,13 @@
+import Root from '$lib/components/ui/avatar/avatar.svelte';
+import Image from '$lib/components/ui/avatar/avatar-image.svelte';
+import Fallback from '$lib/components/ui/avatar/avatar-fallback.svelte';
+
+export {
+	Root,
+	Image,
+	Fallback,
+	//
+	Root as Avatar,
+	Image as AvatarImage,
+	Fallback as AvatarFallback
+};
